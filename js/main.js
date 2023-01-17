b5ce70ed65aa6bb909063e7bcab7e84b26805edc
D.Tamilarasan <abidm26791@gmail.com>
2023-01-16 20:17:09 -0800

    js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+										// ODD OR EVEN NUMBER
+var n = parseInt(prompt("Enter the value"))
+if(n%2==0){
+    document.write("The number"+n+" is Even Number")
+}
+else{
+    document.write("The number"+n+"odd Number")
+}
